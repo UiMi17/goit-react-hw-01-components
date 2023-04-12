22a3dbe3588d4b5846bf98701934e98312d7ece1
Dan Dudaiev <roffrooff@gmail.com>
2023-04-12 18:35:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 // Task 1
-import user from '../user-data/user';
-import { Profile } from './user-profile/User-profile';
+import user from '../user-data/user.json';
+import { Profile } from './user-profile/User-profile.jsx';
 // Task 2
-import stats from '../statistics-data/data';
-import { Statistics } from './statistic/Statistics';
+import stats from '../statistics-data/data.json';
+import { Statistics } from './statistic/Statistics.jsx';
 // Task 3
-import friends from '../friends-list-data/friends';
-import { FriendList } from './user-friends/Friend-list';
+import friends from '../friends-list-data/friends.json';
+import { FriendList } from './user-friends/Friend-list.jsx';
 // Task 4
-import transactions from '../transactions-data/transactions';
-import { TransactionHistory } from './transaction-history/Transactions';
+import transactions from '../transactions-data/transactions.json';
+import { TransactionHistory } from './transaction-history/Transactions.jsx';
 
 export const App = () => {
   return (
